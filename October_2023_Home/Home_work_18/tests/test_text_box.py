b793cf537587683ece14ba2550fb18c7e30b598b
Olena Kravtsova <kravcovaalena91@gmail.com>
2024-01-17 19:26:55 +0200

    Merge remote-tracking branch 'origin/main'

--- October_2023_Home/Home_work_18/tests/test_text_box.py
+++ October_2023_Home/Home_work_18/tests/test_text_box.py
@@ -14,48 +14,41 @@ class TestTextBox:
     #     page.click_submit()
     #     pass
 
-    # def test_username_fill_and_check(self, chrome):
-    #     page = TextBoxPage(chrome)
-    #     page.open()
-    #     page.fill_full_name_field("Olena")
-    #     page.scroll_down()
-    #     page.click_submit()
-    #     name_field = page.get_result_fullname()
-    #     assert name_field.replace("Name:", "") == "Olena"
-    #
-    # def test_email_fill_and_check(self, chrome):
-    #     page = TextBoxPage(chrome)
-    #     page.open()
-    #     page.fill_full_email_field("Sallita@i.ua")
-    #     page.scroll_down()
-    #     page.click_submit()
-    #     email_field = page.get_result_email()
-    #     assert email_field.replace("Email:", "") == "Sallita@i.ua"
+    def test_username_fill_and_check(self, chrome):
+        page = TextBoxPage(chrome)
+        page.open()
+        page.fill_full_name_field("Olena")
+        page.scroll_down()
+        page.click_submit()
+        name_field = page.get_result_fullname()
+        assert name_field.replace("Name:", "") == "Olena"
 
-    # def test_curr_addr_fill_and_check(self, chrome):
-    #     page = TextBoxPage(chrome)
-    #     page.open()
-    #     page.fill_current_address_field("Test current address")
-    #     page.scroll_down()
-    #     page.click_submit()
-    #     current_address_field = page.get_result_current_address()
-    #     assert current_address_field == "Test current address"
+    def test_email_fill_and_check(self, chrome):
+        page = TextBoxPage(chrome)
+        page.open()
+        page.fill_email_field("email@gmail.com")
+        page.scroll_down()
+        page.click_submit()
+        email_field = page.get_result_email()
+        assert email_field.replace("Email:", "") == "email@gmail.com"
 
-        def test_curr_addr_fill_and_check(self, chrome):
-            page = TextBoxPage(chrome)
-            page.open()
-            page.fill_current_address_field("Test current address")
-            page.scroll_down()
-            page.click_submit()
-            current_address_field = page.get_result_current_address()
-            assert "Test current address" in current_address_field
+    def test_curr_addr_fill_and_check(self, chrome):
+        page = TextBoxPage(chrome)
+        page.open()
+        page.fill_current_address_field("current address")
+        page.scroll_down()
+        page.click_submit()
+        curr_addr_field = page.get_result_current_address()
+        assert curr_addr_field.replace("Current Address :", "") == "current address"
 
-    # def test_perm_addr_fill_and_check(self, chrome):
-    #     page = TextBoxPage(chrome)
-    #     page.open()
-    #     page.fill_permanent_address_field("Test permanent address")
-    #     page.scroll_down()
-    #     page.click_submit()
-    #     permanent_address_field = page.get_result_permanent_address
-    #     assert permanent_address_field == "Test permanent address"
+    def test_perm_addr_fill_and_check(self, chrome):
+        page = TextBoxPage(chrome)
+        page.open()
+        page.fill_permanent_address_field("permanent address")
+        page.scroll_down()
+        page.click_submit()
+        perm_addr_field = page.get_result_permanent_address()
+        assert perm_addr_field.replace("Permananet Address :", "") == "permanent address"
+
+
 
