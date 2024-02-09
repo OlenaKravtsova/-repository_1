54b609df9b77d8c16bc5946a7938d675003e86bf
Olena <149534308+OlenaKravtsova@users.noreply.github.com>
2024-02-09 19:19:40 +0200

    Update test_buttons.py

--- October_2023_Home/Home_work_24/tests/test_buttons.py
+++ October_2023_Home/Home_work_24/tests/test_buttons.py
@@ -13,21 +13,17 @@ class TestButtons:
         self.driver: WebDriver = self.driver
         self.page = PageButtons(driver=self.driver)
 
-    # def test_doubleclick_button(self):
-    #     self.page.open()
-    #     self.page.button_doubleclick().doubleclick()
-    #     # Встановлює явне очікування протягом 5 секунд на видимість елемента
-    #     WebDriverWait(self.driver, timeout=5).until(EC.visibility_of_element_located(self.page.button_doubleclick_message_loc))
-    #     assert self.page.get_button_doubleclick_message() == "You have done a double click"
-    #
-    # def test_right_click_button(self):
-    #     self.page.open()
-    #     self.page.button_right_click().right_click()
-    #     WebDriverWait(self.driver, timeout=5).until(EC.visibility_of_element_located(self.page.button_right_click_message_loc))
-    #     assert self.page.get_button_right_click_message() == "You have done a right click"
+    def test_doubleclick_button(self):
+        self.page.open()
+        self.page.button_doubleclick().doubleclick()
+        assert self.page.get_button_doubleclick_message() == "You have done a double click"
+
+    def test_right_click_button(self):
+        self.page.open()
+        self.page.button_right_click().right_click()
+        assert self.page.get_button_right_click_message() == "You have done a right click"
 
     def test_dynamic_id_click_button(self):  #кнопка Click Me
         self.page.open()
-        self.page.button_dynamic_id_click().click()  # Assuming you have a method for dynamic click
-        WebDriverWait(self.driver, timeout=5).until(EC.visibility_of_element_located(self.page.button_dynamic_id_click_message_loc))
+        self.page.button_dynamic_id_click().click()
         assert self.page.get_button_dynamic_id_click_message() == "You have done a dynamic click"
