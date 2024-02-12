b1b943b242a8744282b11faf187b6c4ca4771eaa
Olena Kravtsova <kravcovaalena91@gmail.com>
2024-02-13 01:36:38 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	October_2023_Home/Home_work_24/tests/test_buttons.py
    #	October_23/lesson_25/tests/conftest.py

--- October_2023_Home/Home_work_22/exchange_rates.py
+++ October_2023_Home/Home_work_22/exchange_rates.py
@@ -4,9 +4,11 @@ from datetime import datetime
 
 
 def get_exchange_rates():
+    """Ця функція використовує бібліотеку requests, щоб виконати GET-запит до API НБУ за вказаним URL.
+     Якщо статус відповіді дорівнює 200 (OK), тоді вона повертає вміст відповіді у форматі JSON.
+     У протилежному випадку генерується виняток Exception з повідомленням про помилку."""
     URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
-    response = requests.get(URL)
-
+    response = requests.request(method="GET", url=URL)
     if response.status_code == 200:
         return response.json()
     else:
@@ -14,6 +16,9 @@ def get_exchange_rates():
 
 
 def write_to_file(data, filename="exchange_rates.txt"):
+    """Ця функція призначена для запису отриманих курсів валют у текстовий файл.
+    Вона створює файл із заданим ім'ям "exchange_rates.txt" і записує дату та час створення запиту.
+    Потім для кожної валюти з отриманих даних вона записує рядок у файл, що містить назву валюти та її курс до гривні."""
     with open(filename, "w") as file:
         file.write(f"[дата створення запиту] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
 
@@ -22,12 +27,18 @@ def write_to_file(data, filename="exchange_rates.txt"):
 
 
 def run():
+    """Ця функція викликає get_exchange_rates() для отримання курсів валют і передає їх у write_to_file() для запису у файл.
+     Якщо виникає помилка типу requests.exceptions.RequestException, виводиться відповідне повідомлення.
+     Якщо виникає будь-яка інша помилка, то виводиться загальне повідомлення про помилку."""
     try:
         exchange_rates = get_exchange_rates()
         write_to_file(exchange_rates)
         print("Курси валют успішно записані у файл.")
+    except requests.exceptions.RequestException as e:
+        print(f"Не вдалося зробити запит для отримання курсів валют. Помилка: {e}")
     except Exception as e:
-        print(f"Не вдалося отримати курси валют. Помилка: {e}")
+        print(f"Сталася інша помилка при виконанні програми. Помилка: {e}")
 
-# Викликати функцію безпосередньо, якщо скрипт запущено
-    run()
+
+"""Викликається функція безпосередньо, якщо скрипт запущено."""
+run()
