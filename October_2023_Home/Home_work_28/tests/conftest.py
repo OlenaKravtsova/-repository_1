a74862d97488dce5ce9927eca34d48b958663fc8
Olena Kravtsova <kravcovaalena91@gmail.com>
2024-02-27 01:09:39 +0200

    Merge remote-tracking branch 'origin/main' update

--- October_2023_Home/Home_work_28/tests/conftest.py
+++ October_2023_Home/Home_work_28/tests/conftest.py
@@ -2,11 +2,13 @@ import pytest
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def chrome():
-    s = Service(r'C:\Testgit\Hellel\October_2023_Home\Home_chrome\chromedriver.exe')
+    s = Service(r'/Hellel/October_23_Home/Home_chrome/chromedriver.exe')
     driver = webdriver.Chrome(service=s)
     yield driver
     driver.quit()
@@ -14,8 +16,11 @@ def chrome():
 
 @pytest.fixture(scope="class")
 def firefox(request):
-    s = Service(r'C:\Testgit\Hellel\October_2023_Home\Home_chrome\geckodriver.exe')
-    driver = webdriver.Firefox(service=s)
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
+    # якщо використовуємо драйвер менеджер, то нам не потрібно викачувати мануально наш драйвер
+    # s = Service(r'/Hellel/October_23/Chrom/geckodriver.exe')
+    # driver = webdriver.Firefox(service=s)
     request.cls.driver = driver
     driver.implicitly_wait(5)
     yield driver
@@ -24,14 +29,16 @@ def firefox(request):
 
 @pytest.fixture(scope="class")
 def chrome_class(request):
-    s = Service(r'C:\Testgit\Hellel\October_2023_Home\Home_chrome\chromedriver.exe')
-    driver = webdriver.Chrome(service=s)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    # якщо використовуємо драйвер менеджер, то нам не потрібно викачувати мануально наш драйвер
+    # s = Service(r'/Hellel/October_23/Chrom/chromedriver.exe')
+    # driver = webdriver.Chrome(service=s)
     request.cls.driver = driver
     yield driver
     driver.quit()
 
-
-# фікстура яка приймає декілька параметрів.
+#фікстура яка приймає декілька параметрів.
 @pytest.fixture(scope="class", params=['fashion', 'food', 'history'])
 def fixture_chuck_category(request):
     category = request.param
